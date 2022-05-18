032646a5af7086ece4b06d2c09f63a8a0d8b2579
carson mulligan <carsonmulligan45@gmail.com>
2022-05-18 14:30:43 +0100

    FEAT: BIN rake working

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,11 +1,14 @@
+# frozen_string_literal: true
+
 class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
 
   validates :name, presence: true
   validates :address, presence: true
   validates :phone_number, presence: true
   validates :category, presence: true
 
-  CUISINE = %w(chinese italian japanese french belgian)
+  CUISINE = %w[chinese italian japanese french belgian].freeze
   validates :category, inclusion: { in: CUISINE }
   # TODO: go to routes to validate collection of cuisine types
   # ["chinese", "italian", "japanese", "french", "belgian"].
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   get 'restaurants/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :restaurants
+  resources :restaurants, except: %i[update destroy] do
+    resources :reviews, only: %i[new create]
+  end
+  resources :reviews, only: :destroy
 end
